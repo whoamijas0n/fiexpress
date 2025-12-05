79ea07ec3c6bb22f961ab0d91c576335f0a8ff13
rousemary <rousemary@DESKTOP-5AD1KOK>
2025-12-04 19:03:05 -0600

    se agrego la funcion de exportar estadisticas

--- fiexpress/obj/Debug/net8.0/fiexpress.AssemblyInfo.cs
+++ fiexpress/obj/Debug/net8.0/fiexpress.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fiexpress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86fa5bba54f01705c0a79d95dca52bd6d0b519d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75586c643ffbf897564bd4c065171e1bdf21a1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("fiexpress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fiexpress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
